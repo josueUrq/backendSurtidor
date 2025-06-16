1561375d97ceedf3afe08b0ea074db6c196a14b3
urquidijo <josueurquidicarrillo@gmail.com>
2025-06-16 15:33:26 -0400

    ajuste

--- src/controllers/historialVentas.controller.js
+++ src/controllers/historialVentas.controller.js
@@ -5,6 +5,7 @@ export const getHistorialVentas = async (req, res) => {
   try {
     const result = await pool.query(`
   SELECT 
+    nv.id,
     nv.codigo,
     c.nombre AS cliente_nombre,
     nv.cantidad,
@@ -15,9 +16,6 @@ export const getHistorialVentas = async (req, res) => {
   LEFT JOIN cliente c ON nv.id_cliente = c.id
   ORDER BY nv.created_at DESC
 `);
-
-
-
     res.json(result.rows);
   } catch (error) {
     console.error("Error al obtener historial de ventas:", error);
@@ -28,12 +26,15 @@ export const getHistorialVentas = async (req, res) => {
 export const deleteVenta = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query("DELETE FROM nota_venta WHERE id = $1 RETURNING *", [id]);
-    if (result.rowCount === 0) return res.status(404).json({ error: "Venta no encontrada" });
+    const result = await pool.query(
+      "DELETE FROM nota_venta WHERE id = $1 RETURNING *",
+      [id]
+    );
+    if (result.rowCount === 0)
+      return res.status(404).json({ error: "Venta no encontrada" });
     res.json({ message: "Venta eliminada correctamente" });
   } catch (error) {
     console.error("Error al eliminar venta:", error);
     res.status(500).json({ error: "Error al eliminar venta" });
   }
 };
-
